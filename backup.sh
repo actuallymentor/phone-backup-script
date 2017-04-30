a370f37eccff7105d6a282ed6e66b2c3b7462cb9
Mentor <mentor@palokaj.co>
2017-04-30 13:22:21 +0100

    Add timestamp to folder

--- backup.sh
+++ backup.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+now=$(date)
 mkdir backups
 mkdir backups/TB
 # Copy camscanner scans
@@ -8,17 +9,17 @@ adb pull /sdcard/Download/ backups
 # Copy audiobook reader data
 adb pull /sdcard/ListenAudiobookPlayer/ backups
 # Copy relevant Titanium Backup files
-adb pull /sdcard/TitaniumBackup/co.touchlab.android.onesecondeveryday-* backups/TB
-adb pull /sdcard/TitaniumBackup/com.android.calllogbackup* backups/TB
-adb pull /sdcard/TitaniumBackup/com.google.android.apps.authenticator2* backups/TB
-adb pull /sdcard/TitaniumBackup/com.mycelium.wallet* backups/TB
-adb pull /sdcard/TitaniumBackup/com.oneplus.wifiapsettings* backups/TB
-adb pull /sdcard/TitaniumBackup/com.skvalex.callrecorder* backups/TB
-adb pull /sdcard/TitaniumBackup/com.vito.lux* backups/TB
-adb pull /sdcard/TitaniumBackup/com.whatsapp* backups/TB
-adb pull /sdcard/TitaniumBackup/de.blinkt.openvpn* backups/TB
-adb pull /sdcard/TitaniumBackup/org.adaway* backups/TB
-adb pull /sdcard/TitaniumBackup/org.exobel.routerkeygen* backups/TB
+adb pull /sdcard/TitaniumBackup/co.touchlab.android.onesecondeveryday-* backups/$now/TB
+adb pull /sdcard/TitaniumBackup/com.android.calllogbackup* backups/$now/TB
+adb pull /sdcard/TitaniumBackup/com.google.android.apps.authenticator2* backups/$now/TB
+adb pull /sdcard/TitaniumBackup/com.mycelium.wallet* backups/$now/TB
+adb pull /sdcard/TitaniumBackup/com.oneplus.wifiapsettings* backups/$now/TB
+adb pull /sdcard/TitaniumBackup/com.skvalex.callrecorder* backups/$now/TB
+adb pull /sdcard/TitaniumBackup/com.vito.lux* backups/$now/TB
+adb pull /sdcard/TitaniumBackup/com.whatsapp* backups/$now/TB
+adb pull /sdcard/TitaniumBackup/de.blinkt.openvpn* backups/$now/TB
+adb pull /sdcard/TitaniumBackup/org.adaway* backups/$now/TB
+adb pull /sdcard/TitaniumBackup/org.exobel.routerkeygen* backups/$now/TB
 # Copy whatsapp data
 adb pull /sdcard/WhatsApp/ backups
 # Copy phone recording data
